2d620cf148f27650da672c3d0c3415608481b095
Paweł Dymek <melasa1998@gmail.com>
2023-11-12 00:00:38 +0100

    Initial Commit

---
+++ Sudoku-Solver/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Cell
+{
+    public List<int> PossibleNumbers { get; set; }
+    public int Value { get; set; }
+    public Cell()
+	{
+        Value = -1;
+        PossibleNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	}
+
+    public Cell(int value)
+    {
+
+    }
+
+    public void Remove(int number)
+    {
+        PossibleNumbers.Remove(number);
+    }
+}
---
+++ Sudoku-Solver/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+StreamReader reader = new StreamReader("F:\\dotNETProjekty\\Sudoku-Solver\\Sudoku-Solver\\sudoku.txt");
+int[,] sudoku = new int[9, 9];
+for (int i = 0; i < 9; i++)
+{
+    for (int j = 0; j < 9; j++)
+    {
+        sudoku[i, j] = -1;
+    }
+}
+int lineNum = -1;
+string line;
+while ((line = reader.ReadLine()) != null)
+{
+    //Console.WriteLine(line);
+    lineNum++;
+    string[] numbers = line.Split(' ');
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        sudoku[lineNum, i] = int.Parse(numbers[i]);
+    }
+}
+
+for(int i = 0; i < 9; i++)
+{
+    for (int j = 0; j < 9; j++)
+    {
+        if (sudoku[i, j] != -1)
+            Console.Write(sudoku[i, j] + " ");
+        else
+            Console.Write("  ");
+    }
+    Console.WriteLine();
+}
+    
+reader.Close();
+Console.ReadKey();
